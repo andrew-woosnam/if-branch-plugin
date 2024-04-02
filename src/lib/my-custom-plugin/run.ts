382e2dadb7f683e8eb3369e3888cd97b368e9091
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-02 11:33:36 -0400

    add run script to test plugin execute

---
+++ src/lib/my-custom-plugin/run.ts
@@ -0,0 +1,25 @@
+import { MyCustomPlugin } from ".";
+import { PluginInterface } from "../types/interface";
+import { YourGlobalConfig } from "./types";
+
+async function runPlugin() {
+    const cfg: YourGlobalConfig = {};
+
+    const newModel: PluginInterface = MyCustomPlugin(cfg);
+    const usage = await newModel.execute([
+        {
+            timestamp: '2021-01-01T00:00:00Z',
+            duration: '15s',
+            'cpu-util': 34,
+        },
+        {
+            timestamp: '2021-01-01T00:00:15Z',
+            duration: '15s',
+            'cpu-util': 12,
+        },
+    ]);
+
+    console.log(usage);
+}
+
+runPlugin();
