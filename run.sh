14dd414ea4ef17ca6e5339209e483f84c8fe9a66
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-06 15:19:43 -0400

    add to run.sh: mkdir outputs if not exists

--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ npm link if-branch-plugin
 # Assign the first argument to a variable
 MANIFEST_PATH="$1"
 
+mkdir -p "outputs"
+
 # Run the command with the provided manifest path
 ie --manifest "$MANIFEST_PATH" >outputs/stdout.txt 2>outputs/stderr.txt | sed -n '/{/,$p' | jq . > "outputs/$(basename "$MANIFEST_PATH")"
 
