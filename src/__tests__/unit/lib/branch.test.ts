4d25f4426954ad9f29016e121bcc1b49142cc3b1
josh-swerdlow <jswerd15@gmail.com>
2024-04-07 19:29:43 -0700

    test if I can push to main

--- src/__tests__/unit/lib/branch.test.ts
+++ src/__tests__/unit/lib/branch.test.ts
@@ -4,7 +4,7 @@ describe('lib/branch: ', () => {
   describe('Branch(): ', () => {
     it('has metadata field.', () => {
       const pluginInstance = Branch();
-
+      // testing if i can push
       expect(pluginInstance).toHaveProperty('metadata');
       expect(pluginInstance).toHaveProperty('execute');
       expect(pluginInstance.metadata).toHaveProperty('kind');
