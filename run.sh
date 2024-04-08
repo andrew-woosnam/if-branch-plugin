d9ef718a88e722499e2ee79d33ce4bce63259566
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-08 17:06:44 -0400

    refactor: improve run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
-# Check if an argument was provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 path/to/manifest.yml"
-    exit 1
-fi
+build_and_link_packages() {
+    npm run build
+    npm link
+    npm link if-branch-plugin
+}
 
-npm run build
-npm link
-npm link if-branch-plugin
+check_yq_installed() {
+    if ! command -v yq &> /dev/null; then
+        echo "yq could not be found, please install it."
+        exit 1
+    fi
+}
 
-# Assign the first argument to a variable
+convert_json_to_yaml() {
+    local input_file="$1"
+    local output_file="$2"
+
+    check_yq_installed
+
+    yq -y . "$input_file" > "$output_file"
+    if [ $? -eq 0 ]; then
+        echo "Conversion successful. The YAML file is saved as result.yml."
+    else
+        echo "json -> yaml conversion failed."
+    fi
+}
+
+extract_json_content() {
+    local input_file="$1"
+    local output_file="$2"
+    sed -n '/^{/,$p' "$input_file" > "$output_file"
+}
+
+
 MANIFEST_PATH="$1"
+STDOUT_FILE="outputs/stdout.txt"
+STDERR_FILE="outputs/stderr.txt"
+RESULT_JSON="outputs/result.json"
+RESULT_YAML="outputs/result.yml"
 
+# Create the outputs directory if it doesn't exist
 mkdir -p "outputs"
 
-# Run the command with the provided manifest path
-ie --manifest "$MANIFEST_PATH" >outputs/stdout.txt 2>outputs/stderr.txt 
+build_and_link_packages
 
-# Use jq to parse the JSON and then convert it to YAML with yq
-cat outputs/stdout.txt | sed -n '/{/,$p' | jq . -r | yq e -P - > "outputs/$(basename "$MANIFEST_PATH" .yml).yml"
+ie --manifest "$MANIFEST_PATH" > "$STDOUT_FILE" 2> "$STDERR_FILE"
 
-# Check if the YAML file was created successfully
-if [ ! -s "outputs/$(basename "$MANIFEST_PATH" .yml).yml" ]; then
-    echo "The YAML file was not created successfully."
-    exit 3
-fi
+extract_json_content "$STDOUT_FILE" "$RESULT_JSON"
+
+convert_json_to_yaml "$RESULT_JSON" "$RESULT_YAML"
