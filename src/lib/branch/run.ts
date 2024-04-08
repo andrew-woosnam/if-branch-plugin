f7f266f68ff939f29738d3f5f7f5a0476975f20b
josh-swerdlow <joshua.swerdlow@yale.edu>
2024-04-07 17:38:16 -0700

    Config changes

--- src/lib/branch/run.ts
+++ src/lib/branch/run.ts
@@ -1,23 +1,29 @@
 import {Branch} from '.';
 import {PluginInterface} from '../types/interface';
-import {YourGlobalConfig} from './types';
 
 async function runPlugin() {
-  const cfg: YourGlobalConfig = {};
-
-  const newModel: PluginInterface = Branch(cfg);
-  const usage = await newModel.execute([
+  const newModel: PluginInterface = Branch();
+  const usage = await newModel.execute(
+    [
+      {
+        timestamp: '2021-01-01T00:00:00Z',
+        duration: '15s',
+        'cpu-util': 34,
+        region: 'uk-south',
+      },
+      {
+        timestamp: '2021-01-01T00:00:15Z',
+        duration: '15s',
+        'cpu-util': 12,
+        region: 'uk-south',
+      },
+    ],
     {
-      timestamp: '2021-01-01T00:00:00Z',
-      duration: '15s',
-      'cpu-util': 34,
-    },
-    {
-      timestamp: '2021-01-01T00:00:15Z',
-      duration: '15s',
-      'cpu-util': 12,
-    },
-  ]);
+      branch: {
+        region: ['uk-north', 'uk-south'],
+      },
+    }
+  );
 
   console.log(usage);
 }
--- src/lib/types/interface.ts
+++ src/lib/types/interface.ts
@@ -3,7 +3,7 @@ export type PluginParams = Record<string, any>;
 export type PluginInterface = {
   execute: (
     inputs: PluginParams[],
-    config?: Record<string, any>
+    config: Record<string, any>
   ) => Promise<PluginParams[]>;
   metadata: {
     kind: string;
