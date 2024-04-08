265d1710de7bec050e296e81e6861ad9a95b374c
josh-swerdlow <jswerd15@gmail.com>
2024-04-07 20:43:16 -0700

    Unit Tests

---
+++ src/__tests__/unit/lib/branch/index.test.ts
@@ -0,0 +1,80 @@
+import {Branch} from '../../../../lib/branch';
+import {ERRORS} from '../../../../util/errors';
+
+const {InputValidationError} = ERRORS;
+
+describe('lib/branch: ', () => {
+  describe('Branch(): ', () => {
+    const branch = Branch();
+    describe('init : ', () => {
+        it('successfully initialized.', () => {
+            expect(branch).toHaveProperty('metadata');
+            expect(branch).toHaveProperty('execute');
+            expect(branch.metadata).toHaveProperty('kind');
+            expect(typeof branch.execute).toBe('function');
+        });
+    });
+
+    describe('execute(): ', () => {
+      /** Currently does not test for grouping */
+      it('successfully applies branch to given input.', async () => {
+        const expectedResult = [
+            {
+              timestamp: '2021-01-01T00:00:00Z',
+              duration: '15s',
+              'cpu-util': 34,
+              region: 'uk-south',
+            },
+            {
+              timestamp: '2021-01-01T00:00:00Z',
+              duration: '15s',
+              'cpu-util': 34,
+              region: 'uk-north',
+            },
+        ];
+
+        const result = await branch.execute(
+          [
+            {
+              timestamp: '2021-01-01T00:00:00Z',
+              duration: '15s',
+              'cpu-util': 34,
+              region: 'uk-south',
+            },
+          ],
+          {
+            region: ['uk-north'],
+          }
+        );
+
+        expect(result).toStrictEqual(expectedResult);
+      });
+
+      it('throws an error on improperly formed config.', async () => {
+        const expectedMessage = 'Branch: Failed validation';
+
+        expect.assertions(1);
+
+        try {
+          await branch.execute(
+            [
+              {
+                timestamp: '2021-01-01T00:00:00Z',
+                duration: '15s',
+                'cpu-util': 34,
+                region: 'uk-south',
+              },
+            ],
+            {
+              branch: 'region',
+            }
+          );
+        } catch (error) {
+          expect(error).toStrictEqual(
+            new InputValidationError(expectedMessage)
+          );
+        }
+      });
+    });
+  });
+});
