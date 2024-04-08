66adcc3834461acbd8827e86f322c45b6fcf5124
josh-swerdlow <jswerd15@gmail.com>
2024-04-07 22:27:01 -0700

    Implicitly called groupby logic

--- src/__tests__/unit/lib/branch/index.test.ts
+++ src/__tests__/unit/lib/branch/index.test.ts
@@ -18,21 +18,28 @@ describe('lib/branch: ', () => {
     describe('execute(): ', () => {
       /** Currently does not test for grouping */
       it('successfully applies branch to given input.', async () => {
-        const expectedResult = [
-          {
-            timestamp: '2021-01-01T00:00:00Z',
-            duration: '15s',
-            'cpu-util': 34,
-            region: 'uk-south',
+        const expectedResult = {
+          'uk-north': {
+            inputs: [
+              {
+                'cpu-util': 34,
+                duration: '15s',
+                region: 'uk-north',
+                timestamp: '2021-01-01T00:00:00Z',
+              },
+            ],
           },
-          {
-            timestamp: '2021-01-01T00:00:00Z',
-            duration: '15s',
-            'cpu-util': 34,
-            region: 'uk-north',
+          'uk-south': {
+            inputs: [
+              {
+                'cpu-util': 34,
+                duration: '15s',
+                region: 'uk-south',
+                timestamp: '2021-01-01T00:00:00Z',
+              },
+            ],
           },
-        ];
-
+        };
         const result = await branch.execute(
           [
             {
